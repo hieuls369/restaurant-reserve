fc0215d89823279b8d0ce83b50a87139d579a6dd
Nguyễn Hiếu <59680580+hieuls369@users.noreply.github.com>
2022-08-04 14:19:33 +0700

    Revert "review code"

--- tools/response/response.go
+++
@@ -1,7 +0,0 @@
-package response
-
-type ReserveResponse struct {
-	Status  int                    `json:"status"`
-	Message string                 `json:"message"`
-	Data    map[string]interface{} `json:"data"`
-}
